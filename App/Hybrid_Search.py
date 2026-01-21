6ddc5c37438b3710bfa3b94f00bb2b7687184b7a
Hazem-khriji <hazemkhriji0@gmail.com>
2026-01-21 23:13:40 +0100

    added the rerankinig logic to the hybrid search

--- App/Hybrid_Search.py
+++ App/Hybrid_Search.py
@@ -1,10 +1,10 @@
 from qdrant_client import QdrantClient, models
-from sentence_transformers import SentenceTransformer
 
 
 class HybridSearcher:
-    DENSE_MODEL = SentenceTransformer("all-MiniLM-L6-v2")
-    SPARSE_MODEL = "prithivida/Splade_PP_en_v1"
+    DENSE_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+    SPARSE_MODEL = "Qdrant/bm25"
+    LATE_INTERACTION_MODEL = "colbert-ir/colbertv2.0"
 
     def __init__(self, collection_name):
         self.collection_name = collection_name
@@ -14,19 +14,21 @@ class HybridSearcher:
     def search(self, text: str):
         search_result = self.qdrant_client.query_points(
             collection_name=self.collection_name,
-            query=models.FusionQuery(
-                fusion=models.Fusion.RRF
-            ),
             prefetch=[
                 models.Prefetch(
-                    query=self.DENSE_MODEL.encode(text).tolist(),
+                    query=models.Document(text=text, model=self.DENSE_MODEL),
                     using="text-dense",
+                    limit=5
                 ),
                 models.Prefetch(
                     query=models.Document(text=text, model=self.SPARSE_MODEL),
                     using="text-sparse",
+                    limit=5
                 ),
             ],
+            query=models.Document(text=text, model=self.LATE_INTERACTION_MODEL),
+            using="text-late-interaction",
+            with_payload=True,
             query_filter=None,
             limit=5,
         ).points
